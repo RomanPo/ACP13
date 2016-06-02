c03d6c47f2a8a94b51803ad57987841ed561b5a8
r.popov <r.popov@levi9.com>
2016-06-02 15:03:45 +0300

    serializer

---
+++ src/privateserializer/TestReflectionFormater.java
@@ -0,0 +1,19 @@
+package privateserializer;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Created by r.popov on 6/2/2016.
+ */
+public class TestReflectionFormater {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, IOException {
+       ReflectionFormater reflectionFormater = new ReflectionFormater();
+        Car car = new Car("lada", 1989, 2.0);
+        System.out.println(reflectionFormater.format(car));
+        FileOutputStream fl = new FileOutputStream("C:\\Serializer\\src\\privateserializer/test.txt");
+        fl.write(reflectionFormater.format(car).getBytes());
+    }
+}
