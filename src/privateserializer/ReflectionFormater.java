bd335b11ac716c7ca713360fb2a4d82489de2b4c
r.popov <r.popov@levi9.com>
2016-06-02 17:03:33 +0300

    My Homework

--- src/privateserializer/ReflectionFormater.java
+++ src/privateserializer/ReflectionFormater.java
@@ -3,6 +3,8 @@ package privateserializer;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by r.popov on 6/2/2016.
@@ -28,5 +30,49 @@ public class ReflectionFormater {
 
         }
         return stringBuiler.toString();
+    }
+    public Object parse(String src) {
+
+        String[] lines = src.split("\n");
+        String type = lines[0].split(":")[1];
+        Map<String, String> keyValuesMap = new HashMap<>();
+        for (int i = 0; i < lines.length; i++) {
+            String[] keyValue = lines[i].split(":");
+            keyValuesMap.put(keyValue[0], keyValue[1]);
+        }
+        Object instance = null;
+        try {
+            Class cl = Class.forName(type);
+            instance = cl.newInstance();
+            for (String key : keyValuesMap.keySet()) {
+                Field field = cl.getDeclaredField(key);
+
+                Class<?> fieldType = field.getType();
+
+                Method setMethod = cl.getMethod("set" + key.substring(0, 1).toUpperCase() + key.substring(1), fieldType);
+
+                if (int.class == fieldType) {
+                    setMethod.invoke(instance, Integer.parseInt(keyValuesMap.get(key)));
+                } else if (double.class == fieldType) {
+                    setMethod.invoke(instance, Double.parseDouble(keyValuesMap.get(key)));
+                } else if (String.class == fieldType) {
+                    setMethod.invoke(instance, keyValuesMap.get(key));
+                }
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return instance;
     }
 }
